b41a556ba0c73253cb5faf1f1163d0f55ee1aea1
Turgay <turgaj.abdullajev@gmail.com>
2023-03-30 10:05:11 +0400

    Set snake to move

--- main.py
+++ main.py
@@ -1,17 +1,29 @@
 from turtle import Turtle, Screen
+import time
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Snek")
+screen.tracer(0)
 
 starting_position = [(0, 0), (-20, 0), (-40, 0)]
+segments = []
 
 for position in starting_position:
-    snek = Turtle(shape='square')
-    snek.penup()
-    snek.color('white')
-    snek.goto(position)
+    new_segment = Turtle(shape='square')
+    new_segment.speed('fast')
+    new_segment.penup()
+    new_segment.color('white')
+    new_segment.goto(position)
+    segments.append(new_segment)
 
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(0.3)
+    for seg in segments:
+        seg.forward(20)
 
 screen.exitonclick()
